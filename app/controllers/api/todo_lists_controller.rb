0614d77b74bb90a61505156db852b46b08fd0a8f
Jimmy Hsiao <jimmy.h@incubit.co.jp>
2022-08-18 15:54:11 +0800

    add: update todo

--- app/controllers/api/todo_lists_controller.rb
+++ app/controllers/api/todo_lists_controller.rb
@@ -8,7 +8,17 @@ module Api
       TodoList.create!(todo_list_params)
       render json: { todo_lists: TodoList.all }
     rescue
-      render json: { error: 'Create todo list failed.' }, status: :bad_request
+      render json: { error: 'Create todo failed.' }, status: :bad_request
+    end
+
+    def update
+      todo = TodoList.find(params[:id])
+      todo.update!(todo_list_params)
+      render json: { todo_list: todo }
+    rescue ActiveRecord::RecordNotFound
+      render json: { error: 'Record not found' }, status: :not_found
+    rescue
+      render json: { error: 'Update todo failed.' }, status: :bad_request
     end
 
     private
