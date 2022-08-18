cb1aae497d4d0d533b8e9cafb6ddafccb8dbe500
Jimmy Hsiao <jimmy.h@incubit.co.jp>
2022-08-18 15:35:28 +0800

    add: create todo

--- app/controllers/api/todo_lists_controller.rb
+++ app/controllers/api/todo_lists_controller.rb
@@ -3,5 +3,18 @@ module Api
     def index
       render json: { todo_lists: TodoList.all }
     end
+
+    def create
+      TodoList.create!(todo_list_params)
+      render json: { todo_lists: TodoList.all }
+    rescue
+      render json: { error: 'Create todo list failed.' }, status: :bad_request
+    end
+
+    private
+
+    def todo_list_params
+      params.fetch(:todo_list).permit!
+    end
   end
 end
