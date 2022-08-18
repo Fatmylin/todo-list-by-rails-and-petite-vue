c90c9d22ae2f4469ad7b9fff5bd3115162d82496
Jimmy Hsiao <jimmy.h@incubit.co.jp>
2022-08-18 16:20:05 +0800

    add: bulk complete todos

--- app/controllers/api/todo_lists_controller.rb
+++ app/controllers/api/todo_lists_controller.rb
@@ -21,6 +21,14 @@ module Api
       render json: { error: 'Update todo failed.' }, status: :bad_request
     end
 
+    def bulk_complete
+      todos = TodoList.where(id: todo_list_params[:ids])
+      todos.update_all(completed: true)
+      render json: { todo_lists: TodoList.all }
+    rescue => e
+      render json: { error: 'Update todos failed.' }, status: :bad_request
+    end
+
     private
 
     def todo_list_params
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,10 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   resources :todo_lists
-  
+
   namespace :api do 
-    resources :todo_lists
+    resources :todo_lists do 
+      put :bulk_complete, on: :collection
+    end
   end
 end
